2ec2b718536d1b31c156aa1947bc6ff9ae45b275
Asck <asck.bbc@gmail.com>
2019-06-29 22:50:19 +0900

    関数群の移動

---
+++ Software/dataPathes.m
@@ -0,0 +1,8 @@
+pathes.software = strcat(pwd,"\")
+pathes.data = strcat(pathes.software , "Data\")
+pathes.recorder  = strcat(pathes.software , "Recorder\")
+pathes.classifier  = strcat(pathes.software , "Classifier\")
+
+
+
+
