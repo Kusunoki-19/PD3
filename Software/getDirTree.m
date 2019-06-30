8a87ac3b4ae340b0477fe33d3c58f62462143245
Asck <asck.bbc@gmail.com>
2019-06-30 23:51:14 +0900

    dirTree構造の変更

--- Software/getDirTree.m
+++ Software/getDirTree.m
@@ -7,18 +7,21 @@ for i = 1 : length(dirs) %list --> str
 end
 fprintf('\n----- %s -----\n',curPath);
 d = dir(curPath);
-
-j = 0; %file or folder count
+fileNames = string([]); %string list
+fileNum = 0; %file count
+folderNum = 0; %folder count
 for i = 1 : length(d)
     if d(i).name == '.' %except parent directory path
         continue;
     end
-    j = j + 1;
+   
     
     if d(i).isdir == 0 
         % file
         fileName = d(i).name;
-        dirTree{j} = fileName;
+        fileNum = fileNum + 1;
+        fileNames(fileNum) = fileName;
+        
     else
         % directory
         folderName = d(i).name;
@@ -26,8 +29,9 @@ for i = 1 : length(d)
         dirs(end+1) = folderName; %forward to child directory
         [dirs ,childDir] = getDirTree(dirs,{}); %get child directory
         
-        dirTree{j} = struct(folderName,childDir);
+        dirTree = setfield(dirTree, folderName, childDir);
     end
 end
+dirTree = setfield(dirTree, 'files', fileNames);
 dirs = dirs(1:end-1); %back to parent directory
 end
