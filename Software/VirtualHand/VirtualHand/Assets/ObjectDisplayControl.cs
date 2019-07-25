84fc3307706c828070a0dbb0132cdb720aecba7b
Asck <asck.bbc@gmail.com>
2019-07-25 16:26:05 +0900

    UDPスクリプトの整理

--- Software/VirtualHand/VirtualHand/Assets/ObjectDisplayControl.cs
+++ Software/VirtualHand/VirtualHand/Assets/ObjectDisplayControl.cs
@@ -11,13 +11,8 @@ public class ObjectDisplayControl : MonoBehaviour
 {
     private UdpClient receiver;
     private int RECEIVE_PORT = 25000;
-    private string SEND_HOST = "127.0.0.1";
 
-    private UdpClient sender;
-    private int SEND_PORT = 25001;
     int receiveData = 0;
-    GameObject ball;
-    GameObject stick;
 
     private Animator animator;
 
@@ -27,10 +22,10 @@ public class ObjectDisplayControl : MonoBehaviour
         receiver = new UdpClient(RECEIVE_PORT);
         receiver.Client.ReceiveTimeout = 1000;
 
-        sender = new UdpClient();
-        sender.Connect(SEND_HOST, SEND_PORT);
-
         animator = GetComponent<Animator>();
+        animator.SetBool("Gripping", false);
+        animator.SetBool("Picking", false);
+        animator.SetBool("Relaxing", false);
 	}
 	
 	// Update is called once per frame
@@ -38,27 +33,34 @@ public class ObjectDisplayControl : MonoBehaviour
     {
         IPEndPoint remoteEP = null;
         byte[] data = receiver.Receive(ref remoteEP);
-        //receiveData = BitConverter.ToInt32(data, 0);
-        //Debug.Log(data);
-        //Debug.Log(data[0]);
-        switch (data[0])
+        
+        //Debug.Log("data ToStr : " + BitConverter.ToString(data));
+        //Debug.Log("data lengt : " + data.Length);
+        receiveData = int.Parse(BitConverter.ToString(data));
+        //Debug.Log("data ToInt : " + receiveData);
+
+        switch (receiveData)
         {
+            case 0:
+                animator.SetBool("Relaxing", true);
+                animator.SetBool("Gripping", false);
+                animator.SetBool("Picking", false);
+                break;
             case 1:
+                animator.SetBool("Relaxing", false);
                 animator.SetBool("Gripping", true);
                 animator.SetBool("Picking", false);
-                animator.SetBool("Relaxing", false);
                 break;
             case 2:
+                animator.SetBool("Relaxing", false);
                 animator.SetBool("Gripping", false);
                 animator.SetBool("Picking", true);
-                animator.SetBool("Relaxing", false);
                 break;
             default:
+                animator.SetBool("Relaxing", true);
                 animator.SetBool("Gripping", false);
                 animator.SetBool("Picking", false);
-                animator.SetBool("Relaxing", true);
                 break;
         }
-        sender.Send(data, data.Length);
 	}
 }
