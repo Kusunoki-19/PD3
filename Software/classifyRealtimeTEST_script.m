0479a9d36ec9707457e9c1843bf245019125a1d1
Asck <asck.bbc@gmail.com>
2019-08-22 16:24:08 +0900

    変数の整理とNI Deviceのバックグラウンド実行

--- Software/classifyRealtimeTEST_script.m
+++ Software/classifyRealtimeTEST_script.m
@@ -1,16 +1,27 @@
+
+clear
+%sessionデータにいくらのデータが集まった状態でデータを返すか
+%という記述があるので、そのデータを用いてデータ長を決定する
+global GLOBAL_Fs;
+global GLOBAL_Ts;
+GLOBAL_Fs = 2000;
+GLOBAL_Ts = 1/GLOBAL_Fs;
+
 %fetch NI Device
-s = daq.createSession('ni');
-addAnalogInputChannel(s,'Dev1','ai0','Voltage');
+session = daq.createSession('ni');
+addAnalogInputChannel(session,'Dev1','ai0','Voltage');
+
 %set parameters
-s.IsContinuous = true;
-s.Rate=2000;
+session.IsContinuous = true;
+session.Rate = GLOBAL_Fs;
 
-%sessionデータにいくらのデータが集まった状態でデータを返すかという記述があるので
-%そのデータを用いてデータ長を決定する
-signal = zeros(s.NotifyWhenDataAvailableExceeds,1);
+global global_inputSignal;
+global global_outputSignal;
+global_inputSignal  = zeros(session.NotifyWhenDataAvailableExceeds,1);
+global_outputSignal = zeros(session.NotifyWhenDataAvailableExceeds,1);
 
-queueOutputData(signal); %link to output stream data variable
 %plot area setting
+global ax;
 ax = cell(2);
 ax{1} = subplot(1,2,1);
 xlabel(ax{1},'time');
@@ -20,17 +31,33 @@ ax{1}.XLim = [0 10];
 ax{2} = subplot(1,2,2);
 xlabel(ax{1},'time');
 ylabel(ax{1},'Frecency');
-%lh = addlistener(s,'DataAvailable',@callbackDataAvailable);
-lh = addlistener(s,'DataAvailable',@(src,event) src.queueOutputData(signal));
 
+
+%"output" stream
+%queueOutputData(session,[global_inputSignal]);
+%lh = addlistener(s,'DataAvailable'...
+%    ,@(src,event) src.queueOutputData(global_inputSignal));
 
 %session start and end
-startBackground(s);
-delete (lh);
-stop(s);
+listenHandler = addlistener(session,'DataAvailable',@callbackDataAvailable);
+startBackground(session);
+delete (listenHandler);
+stop(session);
+
+function label = refSignalLabel()
+label = classify(TESTNet, spectrogram);
+end
+
+function callbackDataAbailable2()
+cnvSignal = inputSignal.deQ(2000);
+spectrogram = f_signalzconverter(cnvSignal);
+label = refSignalLabel(spectrogram);
+labels = labels
+global_outpuSignal.enQ(labels);
+end
+
 function callbackDataAvailable(src,event)
      %  CALLBACKDATAAVAILABLE called when session data available
-     
      % src , contains session infomation , struct
      % src.NotifyWhenDataAvailableExceeds , data length , double
      
@@ -39,7 +66,22 @@ function callbackDataAvailable(src,event)
      %     double list (src.NotifyWhenDataAvailable)x(channel Len)
      
      %plot(ax{1},event.TimeStamps,event.Data);
+     plot(event.TimeStamps,event.Data);
+     
+     %{
+     persistent dataLen;
+     if isempty(dataLen)
+         dataLen = length(event.data,1);
+     end
+     
+     global_inputSignal.enQ(event.Data);  
      
      plot(event.TimeStamps,event.Data);
+     if waitingLen > convertLen
+        parfeval(callbackDataAvailable2)
+     end
+     
+     labels = global_outputSignal.deQ(dataLen)
+     %}
      
 end
