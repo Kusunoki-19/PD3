72f752c43649882b435091b3a2d5ed79121c0b31
Kusunoki <asck.bbc@gmail.com>
2020-02-03 18:18:48 +0900

    変数の役割コメントの追加

--- Software/LoadValiables.m
+++ Software/LoadValiables.m
@@ -23,21 +23,26 @@ func.subplotter = @(ROW, COL, row, col) subplot(ROW,COL,(row-1)*COL + col);
 param = struct;
 %%
 
+%サンプリング周波数
 param.Fs = 1000;
+%一区間のデータ長[s]
 param.sampleTime = 5;
+%一区間のデータ長
 param.sampleLen = param.Fs * param.sampleTime;
+%チャンネル個数
 param.channelNum = 8;
-
+%実験番号
 param.experiNum = 2;
-
+%学習用ラベル
 param.trainClasses = {'c0','c1','c2'}; %ball, stick, none : c1 c2 c0
+%実験2の学習用ラベル
 if param.experiNum == 2
     %c1~8 , c10~80
     param.validClasses = { ... 
         'c1' ,'c2' ,'c3' ,'c4' ,'c5' ,'c6' ,'c7' ,'c8' , ...
         'c10','c20','c30','c40','c50','c60','c70','c80'}; 
 end
-
+%実験によるデータのパス
 if param.experiNum == 1
     param.isMultiDisplay = false;
     param.dataPath = "D:\kusunoki\PD3\Software\Data\Experimental1";
@@ -50,12 +55,16 @@ param.trainDataPath = param.dataPath;
 param.savePath = strcat(...
     param.dataPath, "\", ...
     datestr(now,"yyyy\\mm\\dd\\HHMM"));
-
+%クリップ前の切り取る信号[s]
 param.firstCutsec = 10;
+%カット周波数(low)[Hz]
 param.cutFreqL = 0;
+%カット周波数(high)[Hz]
 param.cutFreqH = 200;
+%学習データと検証データの割合0~1
 param.trainRate = 0.75;
 
+%figure設定
 param.fig.xlim1 = 9.3;
 param.fig.xlim2 = 13.1;
 param.fig.ylim1 = 0.5;
